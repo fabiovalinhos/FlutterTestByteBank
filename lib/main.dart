c30a273a6eb7005a73865f8c4ec984bc2ddf4b67
fabiovalinhos <fabiovalinhos@gmail.com>
2020-05-17 14:23:53 -0300

    Teste de fluxo

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:bytebank/database/dao/contact_dao.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(BytebankApp());
+  runApp(BytebankApp(
+    contactDao: ContactDao(),
+  ));
 }
 
 class BytebankApp extends StatelessWidget {
+  final ContactDao contactDao;
+
+  BytebankApp({@required this.contactDao});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,7 +24,9 @@ class BytebankApp extends StatelessWidget {
           textTheme: ButtonTextTheme.primary,
         ),
       ),
-      home: Dashboard(),
+      home: Dashboard(
+        contactDao: contactDao,
+      ),
     );
   }
 }
--- test/save_contact_flow.dart
+++ test/save_contact_flow.dart
@@ -5,12 +5,15 @@ import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'dashboard_widget_teste.dart';
 import 'matchers.dart';
+import 'mocks.dart';
 
 void main() {
   testWidgets('Should save a contact', (WidgetTester tester) async {
-    await tester.pumpWidget(BytebankApp());
+    final mockContactDao = MockContactDao();
+    await tester.pumpWidget(BytebankApp(
+      contactDao: mockContactDao,
+    ));
     final dashboard = find.byType(Dashboard);
     expect(dashboard, findsOneWidget);
 
@@ -19,9 +22,9 @@ void main() {
     expect(transferFeatureItem, findsOneWidget);
     //Vamos usar novamente o tester e pegar a função tap
     await tester.tap(transferFeatureItem);
-    await tester.pump();
-    await tester.pump();
-
+//    await tester.pump();
+//    await tester.pump();
+    await tester.pumpAndSettle();
     final contactsList = find.byType(ContactsList);
     expect(contactsList, findsOneWidget);
 
@@ -30,7 +33,38 @@ void main() {
     await tester.tap(fabNewContact);
 //    Fazendo um pump para apresentar o formulário
     await tester.pump();
+//    Não adiantou, vamos usar outra função para resolver = PumpAndSettle
+    await tester.pumpAndSettle();
+//    pumpAndSettle nao adiantou pois API e acessar partes externas exige outra solução: injeção de dependências
     final contactForm = find.byType(ContactForm);
     expect(contactForm, findsOneWidget);
+//  Feito a injeção de dependencias, vamos continuar
+
+    final nameTextField = find.byWidgetPredicate((widget) {
+      if (widget is TextField) {
+        return widget.decoration.labelText == 'Full name';
+      }
+      return false;
+    });
+    expect(nameTextField, findsOneWidget);
+    await tester.enterText(nameTextField, "Fábio");
+
+    final accountNumberTextField = find.byWidgetPredicate((widget) {
+      if (widget is TextField) {
+        return widget.decoration.labelText == 'Account number';
+      }
+      return false;
+    });
+    expect(accountNumberTextField, findsOneWidget);
+    await tester.enterText(accountNumberTextField, '1000');
+
+    final createButton = find.widgetWithText(RaisedButton, 'Create');
+    expect(createButton, findsOneWidget);
+
+    await tester.tap(createButton);
+    await tester.pumpAndSettle();
+
+    final contactsListBack = find.byType(ContactsList);
+    expect(contactsListBack, findsOneWidget);
   });
 }
